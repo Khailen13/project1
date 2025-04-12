fce63869d90cfcdf8b0aa4ab7b97cc881e8445d2
Lenar Khaidarov <haidarov_lenar@mail.ru>
2025-04-12 10:00:37 +0300

    past homework

---
+++ src/masks.py
@@ -0,0 +1,8 @@
+def get_mask_card_number(card_number: int) -> str:
+    """Маскирует номер банковской карты"""
+    return f"{str(card_number)[0:4]} {str(card_number)[4:6]}** **** {str(card_number)[-4:]}"
+
+
+def get_mask_account(account_number: int) -> str:
+    """Маскирует номер счета"""
+    return f"**{str(account_number)[-4:]}"
