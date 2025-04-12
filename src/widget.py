c95fd70967966e8c963d4176f196855c97d6b834
Lenar Khaidarov <haidarov_lenar@mail.ru>
2025-04-12 11:24:54 +0300

    Homework 9.2 to check

--- src/widget.py
+++ src/widget.py
@@ -1,29 +1,29 @@
-from masks import get_mask_card_number
-from masks import get_mask_account
+from masks import get_mask_account, get_mask_card_number
 
-def mask_account_card(type_and_number:str) -> str:
+
+def mask_account_card(type_and_number: str) -> str:
     """Возвращает строку с замаскированным номером"""
 
-    masked_number = ''
+    masked_number = ""
     splited_items = type_and_number.split()
     for item in splited_items:
         if item.isalpha():
-            masked_number += f'{item} '
+            masked_number += f"{item} "
         elif item.isdigit():
             if splited_items[0] == "Счет":
-               masked_number += get_mask_account(item)
+                masked_number += get_mask_account(item)
             else:
-               masked_number += get_mask_card_number(item)
+                masked_number += get_mask_card_number(item)
     return masked_number
 
 
-def get_date(initial_format_date:str) -> str:
+def get_date(initial_format_date: str) -> str:
     """Изменяет формат даты"""
 
-    new_format_date = ''
+    new_format_date = ""
     splited_items = initial_format_date.split("-")
     year = splited_items[0]
     month = splited_items[1]
-    day = splited_items[2].split('T')[0]
+    day = splited_items[2].split("T")[0]
     new_format_date = f"{day}.{month}.{year}"
     return new_format_date
