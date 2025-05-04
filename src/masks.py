fea19c2a28f940f12139f9e329a9cdd80deb3d96
Lenar Khaidarov <haidarov_lenar@mail.ru>
2025-05-04 12:14:37 +0300

    widget&test_widget modified

--- src/masks.py
+++ src/masks.py
@@ -4,7 +4,7 @@ def get_mask_card_number(card_number: str | int) -> str:
     if str(card_number).isdigit() and len(str(card_number)) == 16:
         return f"{str(card_number)[0:4]} {str(card_number)[4:6]}** **** {str(card_number)[-4:]}"
     else:
-        return 'Номер карты введен не правильно'
+        return "Номер карты введен не правильно"
 
 
 def get_mask_account(account_number: str | int) -> str:
@@ -13,6 +13,4 @@ def get_mask_account(account_number: str | int) -> str:
     if str(account_number).isdigit() and len(str(account_number)) == 20:
         return f"**{str(account_number)[-4:]}"
     else:
-        return 'Номер счета введен не правильно'
-
-
+        return "Номер счета введен не правильно"
--- src/widget.py
+++ src/widget.py
@@ -3,44 +3,57 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(type_and_number: str) -> str:
     """Возвращает строку с замаскированным номером"""
-    incorrect_input_message = 'Неправильный ввод данных'
-    if type_and_number == '': # Пустая строка - некорректный ввод
+
+    incorrect_input_message = "Неправильный ввод данных"
+    if type_and_number == "":  # Пустая строка - некорректный ввод
         return incorrect_input_message
     else:
         splited_items = type_and_number.split()
-        parts_count = len(splited_items)
-        if (not (splited_items[0]).isalpha): # Строка начинается не с букв - некорректный ввод
+        if not splited_items[0].isalpha():  # Строка начинается не с букв - некорректный ввод
             return incorrect_input_message
-        else: # Количество цифровой части отлична от единицы - некорректный ввод
+        else:  # Количество цифровой части отлична от единицы - некорректный ввод
             digits_presence = 0
             for item in splited_items:
                 if item.isdigit():
                     digits_presence += 1
             if digits_presence != 1:
                 return incorrect_input_message
-            else: # В остальных случаях
+            else:  # В остальных случаях
                 masked_number = ""
                 for item in splited_items:
-                    if item.isalpha(): # включение буквенной части
+                    if item.isalpha():  # включение буквенной части
                         masked_number += f"{item} "
-                    elif item.isdigit(): # включение цифровой части
+                    elif item.isdigit():  # включение цифровой части
                         if splited_items[0] == "Счет":
                             masked_number += get_mask_account(item)
                         else:
                             masked_number += get_mask_card_number(item)
-                if 'введен не правильно' in masked_number: # Некорректные номера по сообщению модуля masks
+                if "введен не правильно" in masked_number:  # Некорректные номера по сообщению модуля masks
                     return incorrect_input_message
                 else:
                     return masked_number
 
 
 def get_date(initial_format_date: str) -> str:
-    """Изменяет формат даты"""
+    """Принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
+    и возвращает строку с датой в формате "ДД.ММ.ГГГГ" ("11.03.2024")"""
 
-    new_format_date = ""
-    splited_items = initial_format_date.split("-")
-    year = splited_items[0]
-    month = splited_items[1]
-    day = splited_items[2].split("T")[0]
-    new_format_date = f"{day}.{month}.{year}"
-    return new_format_date
+    incorrect_input_message = "Неправильный ввод данных"
+    if len(initial_format_date) < 10:  # если менее 10 символов в строке - некорректный ввод
+        return incorrect_input_message
+    else:
+        year = initial_format_date[0:4]
+        month = initial_format_date[5:7]
+        day = initial_format_date[8:10]
+        if (
+            year.isdigit()
+            and initial_format_date[4] == "-"
+            and month.isdigit()
+            and 1 <= int(month) <= 12
+            and initial_format_date[7] == "-"
+            and day.isdigit()
+            and 1 <= int(day) <= 31
+        ):  # соответствие входного формата
+            return f"{day}.{month}.{year}"
+        else:
+            return incorrect_input_message
