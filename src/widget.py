4828c8614c83b24cc01263016d4522a57835b210
Lenar Khaidarov <haidarov_lenar@mail.ru>
2025-05-04 10:26:43 +0300

    test_widget added & widget modified

--- src/widget.py
+++ src/widget.py
@@ -3,18 +3,35 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(type_and_number: str) -> str:
     """Возвращает строку с замаскированным номером"""
-
-    masked_number = ""
-    splited_items = type_and_number.split()
-    for item in splited_items:
-        if item.isalpha():
-            masked_number += f"{item} "
-        elif item.isdigit():
-            if splited_items[0] == "Счет":
-                masked_number += get_mask_account(item)
-            else:
-                masked_number += get_mask_card_number(item)
-    return masked_number
+    incorrect_input_message = 'Неправильный ввод данных'
+    if type_and_number == '': # Пустая строка - некорректный ввод
+        return incorrect_input_message
+    else:
+        splited_items = type_and_number.split()
+        parts_count = len(splited_items)
+        if (not (splited_items[0]).isalpha): # Строка начинается не с букв - некорректный ввод
+            return incorrect_input_message
+        else: # Количество цифровой части отлична от единицы - некорректный ввод
+            digits_presence = 0
+            for item in splited_items:
+                if item.isdigit():
+                    digits_presence += 1
+            if digits_presence != 1:
+                return incorrect_input_message
+            else: # В остальных случаях
+                masked_number = ""
+                for item in splited_items:
+                    if item.isalpha(): # включение буквенной части
+                        masked_number += f"{item} "
+                    elif item.isdigit(): # включение цифровой части
+                        if splited_items[0] == "Счет":
+                            masked_number += get_mask_account(item)
+                        else:
+                            masked_number += get_mask_card_number(item)
+                if 'введен не правильно' in masked_number: # Некорректные номера по сообщению модуля masks
+                    return incorrect_input_message
+                else:
+                    return masked_number
 
 
 def get_date(initial_format_date: str) -> str:
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,15 +1,15 @@
-from src.masks import get_mask_account, get_mask_card_number
 import pytest
+
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def test_get_mask_card_number():
     incorrect_input_message = "Номер карты введен не правильно"
     assert get_mask_card_number(7000792289606361) == "7000 79** **** 6361"
-    assert get_mask_card_number('7000792289606361') == "7000 79** **** 6361"
+    assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
     assert get_mask_card_number("abcd792289606361") == incorrect_input_message
-    assert get_mask_card_number('abcd') == incorrect_input_message
+    assert get_mask_card_number("abcd") == incorrect_input_message
     assert get_mask_card_number("") == incorrect_input_message
-
 
 
 def test_get_mask_account():
