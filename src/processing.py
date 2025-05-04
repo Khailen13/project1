f3d57d5b8839fc2a9e8b638b376a097acc4c0742
Lenar Khaidarov <haidarov_lenar@mail.ru>
2025-05-04 19:31:51 +0300

    processing & test_processing modified

--- src/processing.py
+++ src/processing.py
@@ -1,35 +1,71 @@
-def filter_by_state(transactions: list, req_state: str = "EXECUTED") -> list:
+def filter_by_state(transactions: list, req_state: str = "EXECUTED") -> list | str:
     """Принимает список словарей и опционально значение для ключа state"""
 
-    incorrect_input_message = "Неправильный ввод данных"
+    incorrect_input_message = "Неправильный ввод данных"  # Сообщение при неправильном вводе
     correct_input = False
-    if len(transactions) > 0: # Непустой список
-        relevant_transcations = []
+    if len(transactions) > 0:  # Непустой список
+        relevant_transcations = []  # Возвращаемый список
         for transaction in transactions:
-            if (("state" in transaction) and (req_state.upper() == "EXECUTED" or req_state.upper() == "CANCELED")): # условия наличия ключа "state", правильности заправшиваемого статуса
+            if ("state" in transaction) and (
+                req_state.upper() == "EXECUTED" or req_state.upper() == "CANCELED"
+            ):  # Наличие ключа "state" и правильность заправшиваемого статуса
                 correct_input = True
-                if transaction["state"].upper() == req_state.upper(): # При соответствии запрашиваемому статусу
+                if transaction["state"].upper() == req_state.upper():  # Соответствие запрашиваемому статусу
                     relevant_transcations.append(transaction)
-        if correct_input:
+        if correct_input:  # При правильном вводе
             return relevant_transcations
-        else:
+        else:  # При отсутствии ключа "state" или ошибке в запрашиваемом статусе
             return incorrect_input_message
-    else:
+    else:  # При пустом входном списке
         return incorrect_input_message
 
 
 def sort_by_date(transactions: list, reverse_order: bool = True) -> list:
-    """принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание)"""
-
-    return sorted(
-        transactions,
-        key=lambda x: (
-            int(x["date"].split("-")[0]),
-            int(x["date"].split("-")[1]),
-            int(x["date"].split("-")[2].split("T")[0]),
-            int(x["date"].split("T")[1].split(":")[0]),
-            int(x["date"].split(":")[1]),
-            float(x["date"].split(":")[2]),
-        ),
-        reverse=reverse_order,
-    )
+    """Принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание)"""
+    # {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    # Проверка соотвествия входного формата
+    incorrect_input_message = "Некорректные данные"
+    correct_transaction_presence = False  # признак наличия в списке транзакции с корректной датой
+    correct_transactions = []  # список транзакций с корректыными датами
+    if len(transactions) == 0:  # пустой список
+        return incorrect_input_message
+    else:
+        for transaction in transactions:
+            if "date" in transaction: # наличие ключа "date"
+                if len(transaction["date"]) >= 18:  # не менее 18 символов - признак корректности данных
+                    year = transaction["date"][0:4]
+                    month = transaction["date"][5:7]
+                    day = transaction["date"][8:10]
+                    hours = transaction["date"][11:13]
+                    minutes = transaction["date"][14:16]
+                    seconds = transaction["date"][17:]
+                    if (
+                        year.isdigit()  # проверка частей даты ожидаемому формату
+                        and transaction["date"][4] == "-"
+                        and month.isdigit()
+                        and 1 <= int(month) <= 12
+                        and transaction["date"][7] == "-"
+                        and day.isdigit()
+                        and 1 <= int(day) <= 31
+                        and transaction["date"][10] == "T"
+                        and 0 <= int(hours) <= 23
+                        and transaction["date"][13] == ":"
+                        and 0 <= int(minutes) <= 59
+                        and transaction["date"][16] == ":"
+                        and 0 <= float(seconds) <= 60
+                    ):
+                        correct_transaction_presence = True
+                        correct_transactions.append(transaction)
+    if correct_transaction_presence:
+        return sorted(
+            correct_transactions,
+            key=lambda x: (
+                int(x["date"].split("-")[0]),
+                int(x["date"].split("-")[1]),
+                int(x["date"].split("-")[2].split("T")[0]),
+                int(x["date"].split("T")[1].split(":")[0]),
+                int(x["date"].split(":")[1]),
+                float(x["date"].split(":")[2]),
+            ),
+            reverse=reverse_order,
+        )
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -2,29 +2,81 @@ import pytest
 
 from src.processing import filter_by_state, sort_by_date
 
+
 def test_filter_by_state_1(initial_transactions):
     incorrect_input_message = "Неправильный ввод данных"
     # Проверка статуса 'EXECUTED' при отсутствии непосредственного ввод статуса
     assert filter_by_state(initial_transactions) == [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
     # Проверка статуса 'EXECUTED' при вводе строчными буквами
-    assert filter_by_state(initial_transactions, 'executed') == [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+    assert filter_by_state(initial_transactions, "executed") == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
     # Проверка статуса 'CANCELED'
-    assert filter_by_state(initial_transactions,"CANCELED") == [
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+    assert filter_by_state(initial_transactions, "CANCELED") == [
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
     # Проверка статуса 'CANCELED' при вводе строчными буквами
     assert filter_by_state(initial_transactions, "canceled") == [
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
 
-#Проверка случаев пустового списка, некорректного ввода статуса, отсутствия ключа "state"
-@pytest.mark.parametrize("transactions, req_state",
-    [([], 'EXECUTED'), ([{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], "abra"), ([{'id': 594226727, 'date': '2018-09-12T21:27:25.241689'},{'id': 615064591, 'date': '2018-10-14T08:21:33.419441'}], 'EXECUTED')])
+
+# Проверка случаев пустового списка, некорректного ввода статуса, отсутствия ключа "state"
+@pytest.mark.parametrize(
+    "transactions, req_state",
+    [
+        ([], "EXECUTED"),
+        (
+            [
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            "abra",
+        ),
+        (
+            [
+                {"id": 594226727, "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "date": "2018-10-14T08:21:33.419441"},
+            ],
+            "EXECUTED",
+        ),
+    ],
+)
 def test_filter_by_state_2(transactions, req_state):
-        incorrect_input_message = "Неправильный ввод данных"
-        assert filter_by_state(transactions, req_state) == incorrect_input_message
+    incorrect_input_message = "Неправильный ввод данных"
+    assert filter_by_state(transactions, req_state) == incorrect_input_message
 
+
+def test_sort_by_date_1(initial_transactions: list, reverse_order: bool = True):
+    # Проверка сортировки по убыванию
+    assert sort_by_date(initial_transactions) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+def test_sort_by_date_2(initial_transactions: list, reverse_order: bool = True):
+    # Проверка сортировки по возрастанию
+    assert sort_by_date(initial_transactions, False) == [
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    ]
+
+def test_sort_by_date_3(same_day_transactions: list, reverse_order: bool = True):
+    # Проверка сортировки по убыванию при одинаковых датах - сортировка по часам-минутам-секундам
+    assert sort_by_date(same_day_transactions) == [
+        {"id": 594226727, "state": "CANCELED", "date": "2019-07-03T21:27:25.241689"},
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2019-07-03T08:21:33.419441"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2019-07-03T02:08:58.425572"},
+    ]
